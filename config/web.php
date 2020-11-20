4e41952050c9e2bad60b9fca7a4593092692b13a
pavel <piwowarow87@yandex.ru>
2020-11-20 22:29:42 +0300

    lesson 5. top-products

--- config/web.php
+++ config/web.php
@@ -54,6 +54,8 @@ $config = [
             'showScriptName' => false,
             'enableStrictParsing' => false,
             'rules' => [
+                'category/<id:\d+>' => 'category/view',
+                'product/<id:\d+>' => 'product/view',
             ],
         ],
 
--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -4,12 +4,15 @@
 namespace app\controllers;
 
 
+use app\models\Product;
+
 class HomeController extends AppController
 {
 //    public $layout = 'grocery'; //or in web config file initialize this
 
     public function actionIndex()
     {
-        return $this->render('index');
+        $offers = Product::find()->where(['is_offer' => 1])->limit(4)->all();
+        return $this->render('index', compact('offers'));
     }
 }
